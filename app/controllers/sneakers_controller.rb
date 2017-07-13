73722a2da364b1009fd1eed494ff7c61477c5a8f
Fred Calhoun <fcalhoun4@yahoo.com>
2017-07-12 21:27:37 -0500

    Current mini-app

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,2 +1,66 @@
 class SneakersController < ApplicationController
+  def index
+    @sneakers = Sneaker.all
+    render "index.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    sneaker = Sneakers.new(
+      brand: params[:form_brand],
+      price: params[:form_price],
+      description: params[:form_description],
+      image: params[:form_image]
+
+    )
+    sneaker.save
+    render "create.html.erb"
+  end
+
+  def show
+    sneaker_id = params[:id]
+    @sneaker = Sneaker.find_by(id: sneaker_id)
+    render "show.html.erb"
+  end
+
+  def edit
+    sneaker_id = params[:id]
+    @sneaker = Sneaker.find_by(id: sneaker_id)
+    render "edit.html.erb"
+  end
+
+  def update
+    sneaker_id = params[:id]
+    @sneaker = Sneaker.find_by(id: sneaker_id)
+    @sneaker.brand = params[:form_brand]
+    @sneaker.price = params[:form_price]
+    @sneaker.description = params[:form_description]
+    @sneaker.image = params[:form_image]
+    @sneaker.save
+    render "update.html.erb"
+  end
+
+  def destroy
+    sneaker_id = params[:id]
+    @sneaker = Sneaker.find_by(id: sneaker_id)
+    @sneaker.destroy
+    render "destroy.html.erb"
+  end
+
+  def test_method
+    @iceberg = Iceberg.find_by(id: params[:id])
+    render "test.html.erb"
+  end
+
+  def sale_message
+    if price.to_f < 2
+      return "Discount item!"
+    else
+      return "Everyday value!"
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/" => "sneakers#index"
+  
+  get "/sneakers" => "sneakers#index"
+  get "/sneakers/new" => "sneakers#new"
+  get "/sneakers" => "sneakers#create" 
+  get "/sneakers/:id" => "sneakers#show"
+  get "/sneakers/:id/edit" => "sneakers#edit"
+  patch "/sneakers/:id" => "sneakers#update"
+  delete "/sneakers/:id" => "sneakers#destroy"
+
+  get "/iceberg/:id" => "sneakers#test_method"
 end
